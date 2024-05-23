63c1c5377ada2cdf237b5cbc1721cd431dbc23a7
lirui <rayestonal@gmail.com>
2024-05-23 16:05:39 +0800

    使用 hardhat 初始化 & 编译 & 测试一个锁定 ether 的 合约

---
+++ eth/web3js/hardhat/scripts/deploy.ts
@@ -0,0 +1,31 @@
+import { web3 } from "hardhat";
+import artifacts from "../artifacts/contracts/Lock.sol/Lock.json"
+
+async function main() {
+    const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+    const unlockTime = currentTimestampInSeconds + 60;
+
+    const lockedAmount = web3.utils.toWei("0.001", 'ether');
+    const [deployer] = await web3.eth.getAccounts();
+    const lockContract = new web3.eth.Contract(artifacts.abi);
+    const rawContract = lockContract.deploy({
+        data: artifacts.bytecode,
+        arguments: [unlockTime],
+    });
+
+    const lock = await rawContract.send({
+        from: deployer,
+        gasPrice: "10000000000",
+        value: lockedAmount.toString()
+    })
+
+    console.log(`Lock with ${web3.utils.toWei(lockedAmount, 'ether')} 
+                ETH and unlock timestamp ${unlockTime} deployed to ${lock.options.address}`);
+
+
+
+    main().catch((error) => {
+        console.error(error);
+        process.exitCode = 1;
+  })
+}
---
+++ eth/web3js/hardhat/test/Lock.ts
@@ -0,0 +1,74 @@
+import {
+  time,
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import { web3 } from "hardhat";
+import artifacts from "../artifacts/contracts/Lock.sol/Lock.json";
+
+describe("Lock", function () {
+  async function deployOneYearLockFixture() {
+    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+    const ONE_GWEI = 1_000_000_000;
+
+    const lockedAmount = ONE_GWEI;
+    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+
+    const lockContract = new web3.eth.Contract(artifacts.abi);
+    lockContract.handleRevert = true;
+
+    const [deployer, otherAccount] = await web3.eth.getAccounts();
+    const rawContract = lockContract.deploy({
+      data: artifacts.bytecode,
+      arguments: [unlockTime],
+    });
+
+    // To know how much gas will be consumed, we can estimate it first.
+    const estimateGas = await rawContract.estimateGas({
+      from: deployer,
+      value: lockedAmount.toString()
+    });
+
+    const lock = await rawContract.send({
+      from: deployer,
+      gas: estimateGas.toString(),
+      gasPrice: "10000000000",
+      value: lockedAmount.toString()
+    });
+
+    console.log("Lock contract deployed to: ", lock.options.address);
+    return { lock, unlockTime, lockedAmount, deployer, otherAccount, rawContract };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right unlockTime", async function () {
+      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+      const setTime = await lock.methods.unlockTime().call();
+      console.log("SetTime", setTime);
+      expect(setTime).to.equal(unlockTime);
+    });
+
+    it("Should set the right deployer", async function () {
+      const { lock, deployer } = await loadFixture(deployOneYearLockFixture);
+
+      expect(await lock.methods.owner().call()).to.equal(deployer);
+    });
+
+    it("Should receive and store the funds to lock", async function () {
+      const { lock, lockedAmount } = await loadFixture(
+        deployOneYearLockFixture
+      );
+      const balance = await web3.eth.getBalance(String(lock.options.address));
+      expect(balance).to.equal(lockedAmount);
+    });
+
+    it("Shouldn't fail if the unlockTime has arrived and the deployer calls it", async function () {
+      const { lock, unlockTime, deployer } = await loadFixture(
+        deployOneYearLockFixture
+      );
+
+      await time.increaseTo(unlockTime);
+      await expect(lock.methods.withdraw().send({ from: deployer })).not.to.be.reverted;
+    });
+  });
+});
