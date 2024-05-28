0fc8354f991df15978c82c00f63e9b9591fb8e0d
lirui <rayestonal@gmail.com>
2024-05-28 16:06:27 +0800

    ts: ts todo demo

--- eth/typescript/freecodecamp/vite/vite-ts-project/src/main.ts
+++ eth/typescript/freecodecamp/vite/vite-ts-project/src/main.ts
@@ -1,24 +1,46 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./css/style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import FullList from "./model/FullList";
+import ListTemplate from "./templates/ListTemplate";
+import ListItems from "./model/ListItems";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const initApp = (): void => {
+  const fullList: FullList = FullList.instance;
+  const listTemplate: ListTemplate = ListTemplate.instance;
+
+  const itemEntryForm = document.getElementById(
+    "itemEntryForm"
+  ) as HTMLFormElement;
+
+  itemEntryForm.addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+    const input = document.getElementById("newItem") as HTMLInputElement;
+    const newEntryText: string = input.value.trim();
+    if (!newEntryText.length) {
+      return;
+    }
+    
+    const itemId: number = fullList.list.length
+      ? parseInt(fullList.list[fullList.list.length - 1].id) + 1
+      : 1;
+
+    const newItem: ListItems = new ListItems(itemId.toString(), newEntryText);
+
+    fullList.additem(newItem);
+    listTemplate.render(fullList);
+  });
+
+  const clearItems = document.getElementById(
+    "clearItemsButton"
+  ) as HTMLButtonElement;
+
+  clearItems.addEventListener("click", () => {
+    fullList.clearList();
+    listTemplate.clear();
+  });
+
+  fullList.load();
+  listTemplate.render(fullList);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
