d6aef68c073528e71c486afa98bc56f14adb22e9
lirui <rayestonal@gmail.com>
2024-05-31 21:50:40 +0800

    hardhat: 部署到 sepolia 测试网络

--- eth/32_hours_demo/hh-fcc/hardhat-simple-storage-fcc/README.md
+++ eth/32_hours_demo/hh-fcc/hardhat-simple-storage-fcc/README.md
@@ -11,4 +11,6 @@ $ yarn hardhat test
 
 $ yarn hardhat run scripts/deploy.js 
 
+# deploy to sepolia network
+$ yarn hardhat run scripts/deploy.js --network sepolia
 ```
--- eth/32_hours_demo/hh-fcc/hardhat-simple-storage-fcc/hardhat.config.js
+++ eth/32_hours_demo/hh-fcc/hardhat-simple-storage-fcc/hardhat.config.js
@@ -1,6 +1,35 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY
+
 module.exports = {
-  solidity: "0.8.24",
-};
+    solidity: "0.8.24",
+
+    // defaultNetwork: "hardhat",
+    networks: {
+        //     ropsten: {
+        //         url: "https://ropsten.infura.io/v3/your-infura-api-key",
+        //         accounts: {
+        //             mnemonic: "your mnemonic here",
+        //         },
+        //     },
+
+        sepolia: {
+            url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: [SEPOLIA_PRIVATE_KEY],
+        },
+    },
+
+    //     rinkeby: {
+    //         url: "https://rinkeby.infura.io/v3/your-infura-api-key",
+    //         accounts: {
+    //             apiKey: PRIVATE_KEY,
+    //         },
+    //         chainId: 4,
+    //     },
+    // },
+}
