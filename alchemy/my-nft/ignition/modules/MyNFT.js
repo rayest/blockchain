375d5ca180f13c90d1dd77a7a01a8f98aee1f79e
lirui <rayestonal@gmail.com>
2024-06-25 14:24:56 +0800

    Revert "alchemy: nft contract"
    This reverts commit 7e6732fc1ba6ef40576b7a4bce6c0e9341467596.

--- alchemy/my-nft/ignition/modules/MyNFT.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-const owner = process.env.WALLET_ADDRESS;
-
-module.exports = buildModule("MyNFTModule", (m) => {
-  const myToken = m.contract("MyNFT", [owner]);
-
-  return { myToken };
-});
