b90350c80c32e1c4d6305ebb3c20bcc0ea72b3bd
lirui <rayestonal@gmail.com>
2024-07-03 17:06:58 +0800

    alchemy: mint nft and load into metamask

--- alchemy/my-nft-front/src/utils/interact.js
+++ alchemy/my-nft-front/src/utils/interact.js
@@ -1,6 +1,6 @@
 import { pinJSONToIPFS } from './pinata.js'
 
-const alchemyKey = "https://www.google.com";
+const alchemyKey = process.env.REACT_APP_ALCHEMY_APP_KEY;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 
@@ -58,7 +58,7 @@ export const mintNFT = async (url, name, description) => {
         return {
             success: true,
             status: (
-                <a target="_blank" href={"https://rinkeby.etherscan.io/tx/" + txHash}>
+                <a target="_blank" href={"https://sepolia.etherscan.io/tx/" + txHash}>
                     View on Etherscan
                 </a>
             ),
