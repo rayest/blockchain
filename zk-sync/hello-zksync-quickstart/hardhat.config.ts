60d6e0fe25c5b4afebcfc8a7238dea654d3988a3
lirui <rayestonal@gmail.com>
2024-07-13 10:41:55 +0800

    zkSync: init a zksync demo project

---
+++ zk-sync/hello-zksync-quickstart/hardhat.config.ts
@@ -0,0 +1,61 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@matterlabs/hardhat-zksync";
+
+require("dotenv").config();
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "zkSyncSepoliaTestnet",
+  networks: {
+    zkSyncSepoliaTestnet: {
+      url: "https://sepolia.era.zksync.dev",
+      ethNetwork: "sepolia",
+      zksync: true,
+      // verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
+    },
+    zkSyncMainnet: {
+      url: "https://mainnet.era.zksync.io",
+      ethNetwork: "mainnet",
+      zksync: true,
+      verifyURL:
+        "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+    },
+
+    dockerizedNode: {
+      url: "http://localhost:3050",
+      ethNetwork: "http://localhost:8545",
+      zksync: true,
+    },
+    inMemoryNode: {
+      url: "http://127.0.0.1:8011",
+      ethNetwork: "localhost", // in-memory node doesn't support eth node; removing this line will cause an error
+      zksync: true,
+    },
+    hardhat: {
+      zksync: true,
+    },
+  },
+
+  zksolc: {
+    // version: "1.5.1", // Uses latest available in https://github.com/matter-labs/zksolc-bin
+    compilerSource: "binary", // 默认即可
+    settings: {
+      compilerPath: "/Users/lirui/Documents/tools/zksolc-macosx-arm64-v1.5.1", // 这里一定放之前下载的zksolc-linux-amd64-musl-v1.3.13路径
+      optimizer: {
+        // 是否使用编译优化，一般都要使用的，因为这可以降低部署的gas
+        enabled: true, // optional. True by default
+        mode: "3", // optional. 3 by default, z to optimize bytecode size
+      },
+    },
+  },
+
+  solidity: {
+    version: "0.8.17",
+  },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
+};
+
+export default config;
