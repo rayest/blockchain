09dadf93da954a3ab3cbe63dbc00f384c4a8ce40
lirui <rayestonal@gmail.com>
2024-07-31 21:27:52 +0800

    Trading with WETH and USDC in local

---
+++ defi/uniswap/trading/src/example/Example.tsx
@@ -0,0 +1,121 @@
+import './Example.css'
+
+import React, { useCallback, useEffect, useState } from 'react'
+
+import { CurrentConfig, Environment } from '../config'
+import {
+  connectBrowserExtensionWallet,
+  getProvider,
+  getWalletAddress,
+  TransactionState,
+} from '../libs/providers'
+import { createTrade, executeTrade, TokenTrade } from '../libs/trading'
+import { displayTrade } from '../libs/utils'
+import { getCurrencyBalance, wrapETH } from '../libs/wallet'
+
+const useOnBlockUpdated = (callback: (blockNumber: number) => void) => {
+  useEffect(() => {
+    const subscription = getProvider()?.on('block', callback)
+    return () => {
+      subscription?.removeAllListeners()
+    }
+  })
+}
+
+const Example = () => {
+  const [trade, setTrade] = useState<TokenTrade>()
+  const [txState, setTxState] = useState<TransactionState>(TransactionState.New)
+
+  const [tokenInBalance, setTokenInBalance] = useState<string>()
+  const [tokenOutBalance, setTokenOutBalance] = useState<string>()
+  const [blockNumber, setBlockNumber] = useState<number>(0)
+
+  // Listen for new blocks and update the wallet
+  useOnBlockUpdated(async (blockNumber: number) => {
+    refreshBalances()
+    setBlockNumber(blockNumber)
+  })
+
+  // Update wallet state given a block number
+  const refreshBalances = useCallback(async () => {
+    const provider = getProvider()
+    const address = getWalletAddress()
+    if (!address || !provider) {
+      return
+    }
+
+    setTokenInBalance(
+      await getCurrencyBalance(provider, address, CurrentConfig.tokens.in)
+    )
+    setTokenOutBalance(
+      await getCurrencyBalance(provider, address, CurrentConfig.tokens.out)
+    )
+  }, [])
+
+  // Event Handlers
+
+  const onConnectWallet = useCallback(async () => {
+    if (await connectBrowserExtensionWallet()) {
+      refreshBalances()
+    }
+  }, [refreshBalances])
+
+  const onCreateTrade = useCallback(async () => {
+    refreshBalances()
+    setTrade(await createTrade())
+  }, [refreshBalances])
+
+  const onTrade = useCallback(async (trade: TokenTrade | undefined) => {
+    if (trade) {
+      setTxState(await executeTrade(trade))
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      {CurrentConfig.rpc.mainnet === '' && (
+        <h2 className="error">Please set your mainnet RPC URL in config.ts</h2>
+      )}
+      {CurrentConfig.env === Environment.WALLET_EXTENSION &&
+        getProvider() === null && (
+          <h2 className="error">
+            Please install a wallet to use this example configuration
+          </h2>
+        )}
+      <h3>
+        Trading {CurrentConfig.tokens.amountIn} {CurrentConfig.tokens.in.symbol}{' '}
+        for {CurrentConfig.tokens.out.symbol}
+      </h3>
+      <h3>{trade && `Constructed Trade: ${displayTrade(trade)}`}</h3>
+      <button onClick={onCreateTrade}>
+        <p>Create Trade</p>
+      </button>
+      <h3>{`Wallet Address: ${getWalletAddress()}`}</h3>
+      {CurrentConfig.env === Environment.WALLET_EXTENSION &&
+        !getWalletAddress() && (
+          <button onClick={onConnectWallet}>Connect Wallet</button>
+        )}
+      <h3>{`Block Number: ${blockNumber + 1}`}</h3>
+      <h3>{`Transaction State: ${txState}`}</h3>
+      <h3>{`${CurrentConfig.tokens.in.symbol} Balance: ${tokenInBalance}`}</h3>
+      <h3>{`${CurrentConfig.tokens.out.symbol} Balance: ${tokenOutBalance}`}</h3>
+      <button
+        onClick={() => wrapETH(100)}
+        disabled={getProvider() === null || CurrentConfig.rpc.mainnet === ''}>
+        <p>Wrap ETH</p>
+      </button>
+      <button
+        onClick={() => onTrade(trade)}
+        disabled={
+          trade === undefined ||
+          txState === TransactionState.Sending ||
+          getProvider() === null ||
+          CurrentConfig.rpc.mainnet === ''
+        }>
+        <p>Trade</p>
+      </button>
+    </div>
+  )
+}
+
+export default Example
